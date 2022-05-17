7d099d9b5e363c1f8fd279451b3a0d656cba0259
Minh Nguyen <thn649@usask.ca>
2022-05-17 15:33:00 -0600

    inital commit

---
+++ components/About.js
@@ -0,0 +1,18 @@
+export default function About() {
+    return (
+        <div className=" text-center">
+            <p>
+                I'm currently a third year Computer Science major at University of Saskatchewan. 
+                During my acedemic years, I have developed interest in Full-stack Development, Human Computer Interaction, and Artificial Intelligence.
+            </p>
+            <br/>
+            <p>I also have a passion for music and arts, synthwave and 80s retro arts, respectively.</p> <br/>
+            <p>Here are some technologies that I have worked with: 
+                Javascript, Next.js, Node.js (Express.js), MongoDB, MySQL, Some web frameworks - Material UI, tailwind CSS (this project!)
+            </p>
+
+        </div>
+  
+
+    )
+}
