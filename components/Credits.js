e8d554037da7ec214d640e7c4ca87e5e103e06e1
Minh Nguyen <thn649@usask.ca>
2022-05-29 10:29:27 -0600

    added stuffs

---
+++ components/Credits.js
@@ -0,0 +1,11 @@
+import style from '../styles/Credits.module.css'
+
+export default function Credits () {
+    return (
+        <div className={style.credits}>
+            <div className={style.endingCredits}>
+                <div>Built and designed by Minh Nguyen. </div>
+            </div>
+        </div>
+    )
+}
