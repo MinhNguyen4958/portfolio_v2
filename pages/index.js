04fa43c3eb83afffb4b0ffde57882ece4ada1787
Minh Nguyen <thn649@usask.ca>
2022-05-29 10:38:10 -0600

    added stuffs again

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import About from '../components/About'
 import Intro from '../components/Intro'
 import Projects from '../components/Projects'
 import SideNavBar from '../components/SideNavBar'
-
+import Credits from '../components/Credits'
 import homeStyle from '../styles/Home.module.css'
 export default function Home() {
   return (
@@ -13,7 +13,8 @@ export default function Home() {
       <div>
         <Intro/>
         <About/>
-        {/* <Projects/> */}
+        <Projects/>
+        <Credits/>
       </div>
       {/* <SideNavBar/> */}
     </div>
