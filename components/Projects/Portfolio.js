4da538c54d06ec4cadffad1dbbb2bda4ce74112b
Minh Nguyen <thn649@usask.ca>
2022-05-30 10:25:34 -0600

    added MUI library

--- components/Projects/Portfolio.js
+++ components/Projects/Portfolio.js
@@ -1,11 +1,17 @@
+import { Button } from "@mui/material";
+
 export default function Portfolio () {
     return (
-    <p>
+        <div>
+            <p>
         This portfolio: I decided to revamp  {' '} 
         <a className='font-bold text-purple-300' href='https://minhnguyen4958.github.io/'> my portfolio </a> 
         with a more modern, miminalistic one. What inspired me to make the change, you may ask? I have fundamental experience
         in Next.js frontend framework and CSS in previous group projects, coupled that with a passion to gain knowledge about UI/UX design thinking.
         <p className=" text-purple-300 font-bold "> Next.js, Tailwind CSS</p>
     </p>
+    <Button/>
+        </div>
+    
     )
 }
