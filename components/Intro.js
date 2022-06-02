4fd2fc43a505168a839f80629ec9126fbe17c8a9
Minh Nguyen <thn649@usask.ca>
2022-06-02 15:41:41 -0600

    changed default text color. Font next

--- components/Intro.js
+++ components/Intro.js
@@ -3,14 +3,14 @@ import style from '../styles/Intro.module.css'
 export default function Intro() {
     return (
         <section id='intro' className={style.introBg}>
-            <div className={style.intro}>
+          <div className={style.intro}>
               <h1 className={style.introTitle}>
                 Welcome! 🪐
               </h1>
               <p className={style.introDesc}>
                 I'm Minh Nguyen. I'm a U of S student.
               </p>
-            </div>
+            </div>   
         </section>
     )
 }
