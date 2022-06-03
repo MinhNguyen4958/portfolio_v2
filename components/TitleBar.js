dc3e862bd6f6517335346a1aa317e76a31839404
Minh Nguyen <thn649@usask.ca>
2022-06-03 15:43:20 -0600

    try with next js recommended font loading method

--- components/TitleBar.js
+++ components/TitleBar.js
@@ -4,6 +4,9 @@ export default function TitleBar() {
     <Head>
         <title>Minh Nguyen</title>
         <link rel="icon" href="/image/Tree.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=NTR&display=swap" rel="stylesheet"/>
     </Head>
     )
 }
