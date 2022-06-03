5d0e41d4d298cf99bf4690cb8492e157d6ba6694
Minh Nguyen <thn649@usask.ca>
2022-06-03 15:46:03 -0600

    fixed

--- components/TitleBar.js
+++ components/TitleBar.js
@@ -5,7 +5,7 @@ export default function TitleBar() {
         <title>Minh Nguyen</title>
         <link rel="icon" href="/image/Tree.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous'/>
         <link href="https://fonts.googleapis.com/css2?family=NTR&display=swap" rel="stylesheet"/>
     </Head>
     )
