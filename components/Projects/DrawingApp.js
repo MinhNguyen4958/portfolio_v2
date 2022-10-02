4ed1c097354ea9883ced2fdb9f7fcee07da317ad
Minh Nguyen <75351231+MinhNguyen4958@users.noreply.github.com>
2022-10-02 16:44:49 -0600

    changed dummy typo

--- components/Projects/DrawingApp.js
+++ components/Projects/DrawingApp.js
@@ -8,7 +8,7 @@ export default function DrawingApp() {
                 <h1>Drawing Application</h1>
                 <p>
                 A basic shape drawing application that is loosely based on MS 
-                Paint. There are shape and color toolbars for users to ultilize, as well as a mini view that 
+                Paint. There are shape and color toolbars for users to utilize, as well as a mini view that 
                 has the same interaction as the main canvas. <br/>
                 </p>
                 <span> Java with JavaFX framework</span>
